c8becd7b3011340b3c07acb220bf36430cbd22c7
berchiche willem <berwillempro@gmail.com>
2024-07-24 12:18:39 +0100

    dark-mode

---
+++ script.js
@@ -0,0 +1,8 @@
+function dark() {
+  document.body.style.backgroundColor = "black";
+  document.querySelector("h1").style.color = "white";
+}
+function light() {
+  document.body.style.backgroundColor = "white";
+  document.querySelector("h1").style.color = "black";
+}
